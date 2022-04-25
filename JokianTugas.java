bde693a9f2ec7e32654f15d10dca638adff15e5e
Aditya Galang Adzana <adityagalang80@gmail.com>
2022-04-25 14:32:26 +0700

    ubah jadi pake replace, jangan pake num

--- JokianTugas.java
+++ JokianTugas.java
@@ -31,7 +31,7 @@ class Main {
         System.out.println("Sort Data : "+ AllChar);
 
         //convert dulu ke number simplify
-        AllChar = ConvertNumber(AllChar);
+        AllChar = SimplifyData(AllChar);
         System.out.println("HASIL SIMPLIFY : "+ AllChar);
 
         //convert lagi ke substractive
@@ -44,180 +44,42 @@ class Main {
         return dataone + datatwo;
     }
 
-    //ini buat convert number untuk di simplify
-    public static String ConvertNumber(String current)
+    // ini buat ubah data simplify
+    public static String SimplifyData(String data)
     {
-        //ini text sebelum
-        char currentText[] = current.toCharArray();
-        // buat simpen data char yang di check
-        char LastCharacter = '\0';
-        // buat simpen data score yang di check
-        int currentScore = 0;
-        // buat simpen data berapa banyak yg di check
-        int maxLength = 0;
+        String CurrentText = data;
 
-        // buat simpen data simplify sementara
-        String TempHasil="";
-
-        //loop dari belakang
-        for(int x = currentText.length-1 ;x >= 0 ; x-- )
+        if(CurrentText.contains("IIIII"))
         {
-            //ketika ini angka pertama yg di check
-            if(x == currentText.length-1)
-            {
-                LastCharacter = currentText[x];
-                currentScore = CalculateScoreNumber(LastCharacter);
-                maxLength += 1;
-
-                TempHasil = "" + LastCharacter;
-            }
-            else
-            {
-                //ketika character yang dicheck sama, berarti tinggal masukin aja lagi ke temp
-                if(LastCharacter == currentText[x])
-                {
-                    TempHasil = LastCharacter + TempHasil;
-                    maxLength += 1;
-                }
-                //ketika character yang di check beda
-                else
-                {
-                    //check simplify sesuai hasil sekarang
-                    TempHasil = CalculateSimplifyNumber(TempHasil, currentScore, maxLength);
-
-                    //check apakah temp yang baru itu beda ga sama yang sekarang , misal CCCC berubah jadi D, nah si D ini sama ga sama yg sekarang kalau sama, berarti tinggal tambahin
-                    if(TempHasil.charAt(0) == currentText[x])
-                    {
-                        LastCharacter = currentText[x];
-                        currentScore = CalculateScoreNumber(LastCharacter);
-                        maxLength = 2;
-                        TempHasil = "" +LastCharacter+TempHasil.charAt(0);
-                    }
-                    // kalau misalnya beda, berarti bikin baru lalu tambahin temp yang tadi ke hasil
-                    else
-                    {
-                        HasilHitungan = TempHasil + HasilHitungan;
-                        LastCharacter = currentText[x];
-                        currentScore = CalculateScoreNumber(LastCharacter);
-                        maxLength = 1;
-                        TempHasil = "" +LastCharacter;
-                    }
-                }
-                // ini misalnya kalau hasil terakhir, langsung check aja lagi
-                if(x == 0)
-                {
-                    TempHasil = CalculateSimplifyNumber(TempHasil, currentScore, maxLength);
-                    HasilHitungan = TempHasil + HasilHitungan;
-                }
-            }
-        }
-        return HasilHitungan;
-    }
-
-    // ini buat check, character yang di check itu 'score'nya berapa, jadi nanti tinggal dikali aja
-    public static int CalculateScoreNumber(char character)
-    {
-        if(character == 'I')
-        {
-            return 1;
-        }
-        if(character == 'V')
-        {
-            return 5;
-        }
-        if(character == 'X')
-        {
-            return 10;
-        }
-        if(character == 'L')
-        {
-            return 50;
-        }
-        if(character == 'C')
-        {
-            return 100;
-        }
-        if(character == 'D')
-        {
-            return 500;
-        }
-        if(character == 'M')
-        {
-            return 1000;
+            CurrentText = CurrentText.replace("IIIII", "V");
         }
 
-        return 0;
-    }
-
-    // ini buat calculate simplify number
-    public static String CalculateSimplifyNumber(String curData, int score,int length)
-    {
-        // ini hitung kemampuan sampai mana simplify berjalan
-        int MaxScore=0;
-        int SelectedRange=0;
-        for(int x=0;x<curData.length();x++)
+        if(CurrentText.contains("VV"))
         {
-            int currentDataScore = CheckNumberSimplify(score,x+1);
-
-            //kalau misalnya lebih berarti naikin kemampuan simplyfi
-            if(currentDataScore >= MaxScore)
-            {
-                MaxScore = currentDataScore;
-                SelectedRange = x+1;
-            }
+            CurrentText =CurrentText.replace("VV", "X");
         }
-        if(MaxScore == score && SelectedRange == length)
+        
+        if(CurrentText.contains("XXXXX"))
         {
-            return curData;
+            CurrentText = CurrentText.replace("XXXXX", "L");
         }
-        //masukin hasil sisa hitungan
-        HasilHitungan = curData.substring(SelectedRange, length) + HasilHitungan;
-
-        if(MaxScore == 1){
-            return "I";
-        }
-        if(MaxScore == 5){
-            return "V";
-        }
-        if(MaxScore == 10){
-            return "X";
-        }
-        if(MaxScore == 50){
-            return "L";
-        }
-        if(MaxScore == 100){
-            return "C";
-        }
-        if(MaxScore == 500){
-            return "D";
-        }
-        if(MaxScore == 1000){
-            return "M";
-        }
-        return curData;
-    }
-    // ini buat check simplify numbernya, check apakah hasilnya ketika di mod number roman itu bakal sama ga, kalau sama tinggal check aja
-    public static int CheckNumberSimplify(int score,int length)
-    {
-        if(score*length % 5 == 0)
+        
+        if(CurrentText.contains("LL"))
         {
-            if(score*length % 10 == 0)
-            {
-                if(score*length % 50 == 0)
-                {
-                    if(score*length % 100 == 0)
-                    {
-                        if(score*length % 500 == 0)
-                        {
-                            if(score*length % 1000 == 0)
-                            {
-                                return 1000;
-                            }   else return 500;
-                        }else return 100;
-                    }else return 50;
-                }else return 10;
-            }else return 5;
-        }else return 1;
+            CurrentText = CurrentText.replace("LL", "C");
+        }
+        
+        if(CurrentText.contains("CCCCC"))
+        {
+            CurrentText = CurrentText.replace("CCCCC", "D");
+        }
+        
+        if(CurrentText.contains("DD"))
+        {
+            CurrentText = CurrentText.replace("DD", "M");
+        }
+
+        return CurrentText;
     }
 
     // ini buat convert subtractive ke addictive
